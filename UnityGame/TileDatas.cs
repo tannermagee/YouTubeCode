caafa8c8d102df4e41b791288563b039c01f7caf
Tanner Magee <39754617+tannermagee@users.noreply.github.com>
2022-05-08 14:02:44 -0600

    Add files via upload

---
+++ UnityGame/TileDatas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class TileDatas : ScriptableObject
+{
+    public List<TileBase> tiles;
+    public float walkingSpeed;
+}
