b05bb1544195626a4ed88c573003d5bf2d372526
Tanner Magee <39754617+tannermagee@users.noreply.github.com>
2022-05-08 14:01:16 -0600

    Add files via upload

---
+++ UnityGame/Interactable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Tilemaps;
+
+[Serializable]
+abstract public class Interactable
+{
+    [SerializeField]
+    public float maxHealth;
+    [SerializeField]
+    public float currentHealth;
+
+    [SerializeField]
+    public Vector3Int location;
+
+    [SerializeField]
+    public bool dropsItem;
+
+    [SerializeField]
+    public Interactable droppedItemInteractable;
+
+    [SerializeField]
+    public TileBase droppedItemTile;
+
+    public void SetHealth(float health)
+    {
+        currentHealth = health;
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public abstract void DropItem();
+
+    public abstract void OnRemoval();
+}
